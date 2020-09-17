b63a242cc0ba534f4c5a1fd3e4e45512ef081931
Mathilde IACHELLA <mathilde.iachella@gmail.com>
2020-09-17 09:18:32 +0200

    TP - Scripting avec Node - Menu

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('** Administration Hotel **');
+var presentation = require('./presentation');
+
+function afficherMenu() {
+    console.log('** Administration Hotel **');
+    presentation.demarrer();
+}
+    afficherMenu();
+    var readline = require('readline');
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+        });
+    rl.question('Votre choix : ', function(saisie) {
+        
+
+        if (saisie == '1'){
+            console.log('>> Liste des clients');
+            rl.close();
+        } else if (saisie == '99') {
+            console.log('Au revoir !');
+            rl.close();
+        } else {
+            console.log('La saisie n\'est pas bonne');
+        }
+    })
+
+
---
+++ presentation.js
@@ -0,0 +1,4 @@
+function start() {
+    console.log('1. Lister les clients \n99. Sortir');
+}
+exports.demarrer = start;
