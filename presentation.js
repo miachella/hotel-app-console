7f0d93c89770b0ca55dd33d6d5e7549ef82aa03d
Mathilde IACHELLA <mathilde.iachella@gmail.com>
2020-09-17 16:14:11 +0200

    TP - Scripting avec Node - Ajouter clients

--- presentation.js
+++ presentation.js
@@ -1,14 +1,14 @@
 var readline = require('readline');
 var service = require("./service.js");
 
-
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+    });
 
 function start() {
-    console.log('1. Lister les clients \n99. Sortir');
-    var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-        });
+    console.log('1. Lister les clients \n2. Ajouter un client \n3. Rechercher un client par nom \n4. Vérifier la disponibilité d\'une chambre \n99. Sortir');
+    
     rl.question('Votre choix : ', function(saisie) {
         switch (saisie){
             case '1':
@@ -17,7 +17,34 @@ function start() {
                     console.log(listerClients.map(function (client) {
                         return client.nom + ' ' + client.prenoms
                     }).join('\n'));
+                    console.log('\n');
+                    start();
+                }, function (err) {
+                    console.log('La liste des clients n\'est pas accessible.');
+                    start();
                 })
+                break;
+            case '2' :
+                console.log('>> Ajout d\'un nouveau client')
+    rl.question('Saisissez le nom du nouveau client : ', function(saisieNom){
+        rl.question('Saisissez le prénom du nouveau client : ', function(saisiePrenom){
+            service.ajouterClient(saisieNom, saisiePrenom, function(ajouterClient){
+                console.log(saisieNom + ' ' + saisiePrenom + ' a correctement été ajouté(e) à la base.')
+                start();
+            }, function (err) {
+                console.log('Le client n\'a pas été ajouté à la base.');
+                start();
+            });
+            
+        })
+    })
+                break;
+            case '3' :
+                console.log('Cette fonction n\'est pas encore disponible, merci de revenir ultérieurement')
+                start();
+                break;
+            case '4' :
+                console.log('Cette fonction n\'est pas encore disponible, merci de revenir ultérieurement')
                 start();
                 break;
             case '99':
--- service.js
+++ service.js
@@ -1,10 +1,23 @@
 var request = require('request');
 
 function listerClients(callbackOK, callbackKO) {
-    request('http://localhost:8080/clients?start=0&size=3', { json: true }, function(err, res, body) {
+    request('http://localhost:8080/clients?start=0&size=10', { json: true }, function(err, res, body) {
         if (err) { callbackKO(err); }
         else { callbackOK(body);}
 });
 }
 
+function ajouterClient(saisieNom, saisiePrenom, callbackOK, callbackKO) {
+    request.post({
+        url:     'http://localhost:8080/clients',
+        method: 'POST',
+        json: {nom: `${saisieNom}`, prenoms: `${saisiePrenom}`}
+      }, function(err, res, body){
+        if (err) { callbackKO(err); }
+        else { callbackOK(body);}
+      });
+}
+
+
 exports.listerClients = listerClients;
+exports.ajouterClient = ajouterClient;
